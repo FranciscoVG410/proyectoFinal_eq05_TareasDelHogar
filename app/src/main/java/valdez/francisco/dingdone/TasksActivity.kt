cc125b6b40e63c23f68470fd117b2523311bedf4
biccsdev <biccsdev@gmail.com>
2025-11-17 21:21:58 -0700

    fixed behavior that showed wrong screen to new user, now showing home config

--- app/src/main/java/valdez/francisco/dingdone/TasksActivity.kt
+++ app/src/main/java/valdez/francisco/dingdone/TasksActivity.kt
@@ -2,12 +2,16 @@ package valdez.francisco.dingdone
 
 import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class TasksActivity : AppCompatActivity() {
 
     private lateinit var bottomNavigationView: BottomNavigationView
+    private val userViewModel: UserViewModel by viewModels()
+    private val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,14 +20,28 @@ class TasksActivity : AppCompatActivity() {
         bottomNavigationView = findViewById(R.id.bottom_navigation)
         bottomNavigationView.itemIconTintList = null
 
-        // Load TasksFragmentNew as the initial fragment
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, TasksFragmentNew())
-                .commit()
+            val userId = auth.currentUser?.uid
+            if (userId != null) {
+                userViewModel.loadUserHomes(userId)
+                userViewModel.userHomes.observe(this) { homes ->
+                    if (homes.isEmpty()) {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container, ConfigurationFragment())
+                            .commit()
+                    } else {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container, TasksFragmentNew())
+                            .commit()
+                    }
+                }
+            } else {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, TasksFragmentNew())
+                    .commit()
+            }
         }
 
-        // Handle back press
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 if (supportFragmentManager.backStackEntryCount > 0) {
